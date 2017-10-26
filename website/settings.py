d603be4ae65a061da64ed733e3ee98150ffbeb07
HarshaVardhan Reddy Kumbhagiri <k.harsha@hotmail.com>
2017-10-26 18:12:17 +0530

    collectstatic

--- website/settings.py
+++ website/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = './dreambuy/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "./dreambuy/static/")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
